3e8e4c20ff681e181605f3cb8c4091a5513e2dbe
fsonic202 <f.oseh@aol.com>
2021-06-20 11:17:06 +0100

    week3 Assesment

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+        var n:Int = 1
+        while(n<=100){
+            when{
+                n%3 == 0 && n%5 == 0 -> println("FizzBuzz")
+                n%3 == 0 -> println("Fizz")
+                n%5 == 0 -> println("Buzz")
+                else -> println(n)
+            }
+            n++
+        }
+    }
